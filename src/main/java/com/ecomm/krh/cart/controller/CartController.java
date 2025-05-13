47888528d850dea25661119b0f2b0c6647c410c8
sudipmondal <sudipmondal81@gmail.com>
2025-05-13 10:17:03 +0530

    log added

--- src/main/java/com/ecomm/krh/cart/controller/CartController.java
+++ src/main/java/com/ecomm/krh/cart/controller/CartController.java
@@ -2,6 +2,8 @@ package com.ecomm.krh.cart.controller;
 
 import com.ecomm.krh.model.CartItem;
 import com.ecomm.krh.services.CartService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,23 +12,26 @@ import java.util.List;
 @RestController
 @RequestMapping("/cart")
 public class CartController {
-
+    private static final Logger logger = LogManager.getLogger(CartController.class);
     @Autowired
     private CartService cartService;
 
-    @PostMapping("/{userId}")
+    @PostMapping("addCart/{userId}")
     public String addToCart(@PathVariable String userId, @RequestBody CartItem item) {
+        logger.info("add to cart ");
         cartService.addToCart(userId, item);
         return "Item added to cart";
     }
 
-    @GetMapping("/{userId}")
+    @GetMapping("getCart/{userId}")
     public List<CartItem> getCart(@PathVariable String userId) {
+        logger.info("get cart call ");
         return cartService.getCart(userId);
     }
 
-    @DeleteMapping("/{userId}")
+    @DeleteMapping("clearCart/{userId}")
     public String clearCart(@PathVariable String userId) {
+        logger.info("clear cart call ");
         cartService.clearCart(userId);
         return "Cart cleared";
     }
